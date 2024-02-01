4a62e0f599cda0eb61ac93d489e5ef9fb4d80db9
Dillon Andreas <dillona@affinitygs.com>
2024-02-01 17:07:38 -0600

    Configure java lsp support (Windows only)

--- lang.el
+++ lang.el
@@ -2,7 +2,17 @@
 ;;; Commentary:
 ;;; java-mode, csv-mode, pascal-mode, markdown-mode, clang-format, cmake-mode, rust-mode, cargo, sql
 ;;; Code:
-;; Java for jsp files
+(use-package dap-mode
+  :ensure t
+  :after lsp-mode
+  :config
+  (dap-mode 1)
+  (dap-ui-mode 1)
+  (dap-tooltip-mode 1)
+  (tooltip-mode 1)
+  (dap-ui-controls-mode 1)
+  (dap-auto-configure-mode))
+; Java for jsp files
 (use-package java-mode
   :after citre
   ; Use global to get reference jumping on java projects, also no auto saves or git gutter to avoid encoding popups
@@ -10,24 +20,20 @@
                        (when (or (eq system-type 'ms-dos) (eq system-type 'windows-nt))
                          (citre-use-global-windows)
                          (setq-local auto-save-default nil)
-                         (git-gutter-mode nil))))
+                         (git-gutter-mode nil)
+                         (require 'dap-java))))
   :mode ("\\.jsp\\'" . java-mode))
-;; (use-package lsp-java
-;;   :ensure t
-;;   :init
-;;   (setq lsp-java-configuration-runtimes '[(:name "JavaSE-1.8"
-;;                                                  :path "C:/Program Files/Java/jdk1.8.0_202"
-;;                                                  :default t)
-;;                                           (:name "JavaSE-17"
-;;                                                  :path "C:/Program Files/Eclipse Adoptium/jdk-17.0.7.7-hotspot")])
-;;   ;; current VSCode defaults
-;;   (setq lsp-java-vmargs '("-XX:+UseParallelGC" "-XX:GCTimeRatio=4" "-XX:AdaptiveSizePolicyWeight=90" "-Dsun.zip.disableMemoryMapping=true" "-Xmx2G" "-Xms100m")))
-;; (use-package dap-mode
-;;   :ensure t
-;;   :after lsp-mode
-;;   :config
-;;   (dap-auto-configure-mode))
-;; (use-package dap-java)
+(use-package lsp-java
+  :ensure t
+  :init
+  (setq lsp-java-java-path "C:/Program Files/Eclipse Adoptium/jdk-17.0.7.7-hotspot/bin/java.exe")
+  (setq lsp-java-configuration-runtimes '[(:name "JavaSE-1.8"
+                                                 :path "C:/Program Files/Java/jdk1.8.0_202"
+                                                 :default t)
+                                          (:name "JavaSE-17"
+                                                 :path "C:/Program Files/Eclipse Adoptium/jdk-17.0.7.7-hotspot")])
+  ;; current VSCode defaults
+  (setq lsp-java-vmargs '("-XX:+UseParallelGC" "-XX:GCTimeRatio=4" "-XX:AdaptiveSizePolicyWeight=90" "-Dsun.zip.disableMemoryMapping=true" "-Xmx2G" "-Xms100m")))
 
 ;; CSV editing mode
 (use-package csv-mode
