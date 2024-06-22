fbb7c37a960d8f197ffe5d3814e9435e4b365a81
zauberin <zauberin@arrakis>
2024-06-22 14:25:27 -0500

    Add exwm config to my base config
    To install exwm, first launch emacs normally outside of exwm, then use
    package-install to install exwm. Next restart will use exwm configuration!

---
+++ exwm.el
@@ -0,0 +1,54 @@
+;;; exwm.el -- exwm configuration
+;;; Commentary:
+;;; Basic exwm configuration (you must manually install exwm to use it)
+;;; Code:
+(use-package exwm
+  :ensure t
+  :demand t
+  :config
+  (require 'exwm-config)
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (display-time-mode)
+  (unless (get 'exwm-workspace-number 'saved-value)
+    (setq exwm-workspace-number 4))
+  ;; Make class name the buffer name
+  (add-hook 'exwm-update-class-hook
+            (lambda ()
+              (exwm-workspace-rename-buffer exwm-class-name)))
+  ;; Global keybindings.
+  (unless (get 'exwm-input-global-keys 'saved-value)
+    (setq exwm-input-global-keys
+          `(
+            ;; 's-r': Reset (to line-mode).
+            ([?\s-r] . exwm-reset)
+            ;; 's-w': Switch workspace.
+            ([?\s-w] . exwm-workspace-switch)
+            ;; 's-&': Launch application.
+            ([?\s-&] . (lambda (command)
+                         (interactive (list (read-shell-command "$ ")))
+                         (start-process-shell-command command nil command)))
+            ;; 's-N': Switch to certain workspace.
+            ,@(mapcar (lambda (i)
+                        `(,(kbd (format "s-%d" i)) .
+                          (lambda ()
+                            (interactive)
+                            (exwm-workspace-switch-create ,i))))
+                      (number-sequence 0 9)))))
+  ;; Line-editing shortcuts
+  (unless (get 'exwm-input-simulation-keys 'saved-value)
+    (setq exwm-input-simulation-keys
+          '(([?\C-b] . [left])
+            ([?\C-f] . [right])
+            ([?\C-p] . [up])
+            ([?\C-n] . [down])
+            ([?\C-a] . [home])
+            ([?\C-e] . [end])
+            ([?\M-v] . [prior])
+            ([?\C-v] . [next])
+            ([?\C-d] . [delete])
+            ([?\C-k] . [S-end delete]))))
+  ;; Enable EXWM
+  (exwm-enable)
+  (push ?\C-w exwm-input-prefix-keys))
+;;; exwm.el ends here
