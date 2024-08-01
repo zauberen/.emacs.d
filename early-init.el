3b41e30ce98553568105a2bd47876c87f91b7c77
zauberin <belfieldcecil@gmail.com>
2024-08-01 13:41:12 -0500

    Linux lsp-booster batteries-included support

--- early-init.el
+++ early-init.el
@@ -16,10 +16,7 @@
       window-resize-pixelwise t)
 
 ;; Set up lsp-mode to use plists (for lsp-booster)
-(if (or (or (eq system-type 'ms-dos) (eq system-type 'windows-nt)
-            (eq system-type 'darwin))
-        (executable-find "emacs-lsp-booster"))
-    (setenv "LSP_USE_PLISTS" "true"))
+(setenv "LSP_USE_PLISTS" "true")
 
 ;; Disable package.el when using elpaca
 (setq package-enable-at-startup nil)
